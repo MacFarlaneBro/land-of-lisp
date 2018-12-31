abf8c970aa55581d04864467aadc536bd32fc382
Charlie MacFarlane Brodie <charliembrodie@gmail.com>
2018-12-31 09:28:50 +0000

    Constructed limited view game map.

--- chapter8.lisp
+++ chapter8.lisp
@@ -76,6 +76,17 @@
     ;; Return the islands
     islands))
 
+(defun find-islands (node edge-list)
+  (let ((islands nil))
+       (labels ((explore-island (unexplored-nodes)
+                  (let* ((connected (get-connected (car unexplored-nodes) edge-list))
+                         (unconnected (set-difference unexplored-nodes connected)))
+                        (push connected islands)
+                        (when unconnected
+                              (explore-island unconnected)))))
+               (explore-island node))
+    islands))
+
 (defun connect-with-bridges (islands)
   ;; If islands has at least two elements
   (when (cdr islands)
@@ -154,3 +165,150 @@
 			    node1-edges))))
 	  ;; pass the edge-alist to the outermost mapcar
 	  edge-alist))
+
+(defun neighbors (node edge-alist)
+  ;; get each edge adjacent to node in edge alist
+  (mapcar #'car (cdr (assoc node edge-alist))))
+
+(defun within-one (a b edge-alist)
+  ;; true if nodes a and b are within one edge of each other false otherwise
+  (member b (neighbors a edge-alist)))
+
+(defun within-two (a b edge-alist)
+  ;; If a and b are within one, return true
+  (or (within-one a b edge-alist)
+      ;; If any of a sequence is not falsy, return true
+      (some
+       (lambda (x)
+	 ;; check to see if element b is a neighbor
+	 (within-one x b edge-alist))
+       ;; for each of the neighbors of element a 
+       (neighbors a edge-alist))))
+
+;; Polish prefix notation is confusing!
+
+(defun make-city-nodes (edge-alist)
+  ;; Set wumpus location to a random node
+  (let ((wumpus (random-node))
+	;; Create a list of nodes of random size for the glow worms
+	(glow-worms (loop for i below *worm-num*
+			  collect (random-node))))
+    ;; Loop through each node
+    (loop for n from 1 to *node-num*
+	  ;; Generate a list from the loop
+	  collect (append
+		   ;; Initialize a list from the node number (this will be the first element of each of the sublists in our alist)
+		   (list n)
+		   
+		   (cond
+		     ;; If this node has the wumpus on then append the wumpus to the node
+		     ((eql n wumpus) '(wumpus))
+		     ;; Otherwise if it is within two edges of the wumpus then append blood
+		     ((within-two n wumpus edge-alist) '(blood!)))
+		   
+		   (cond
+		     ;; If this node has glow worms on then append glow worms
+		     ((member n glow-worms) '(glow-worm))
+		     ;; Otherwise 
+		     ((some (lambda (worm)
+			      ;; If any glow-worm node is within one edge of the current node
+			      (within-one n worm edge-alist))
+			    glow-worms)
+		      ;; append lights
+		      '(lights!)))
+		   ;; If the remainder of the remainder of this node is not empty then there are police on the edge
+		   (when (some #'cdr (cdr (assoc n edge-alist)))
+		     ;; append sirens
+		     '(sirens!))))))
+
+(defun new-game ()
+  (setf *congestion-city-edges* (make-city-edges))
+  (setf *congestion-city-nodes* (make-city-nodes *congestion-city-edges*))
+  (setf *player-pos* (find-empty-node))
+  (setf *visited-nodes* (list *player-pos*))
+  (draw-city)
+  (draw-known-city))
+
+(defun find-empty-node ()
+  ;; Assign x to a random node
+  (let ((x (random-node)))
+    ;; If the cdr of  node x is empty
+    (if (cdr (assoc x *congestion-city-nodes*))
+	;; then find another node
+	(find-empty-node)
+	;; otherwise return node x
+	x)))
+
+
+(defun draw-city ()
+  (ugraph->png "city" *congestion-city-nodes* *congestion-city-edges*))
+
+(defun known-city-nodes ()
+  ;; mapcar 1 
+  (mapcar
+   ;; mapcar 1 function
+   (lambda (node)
+     ;; If the node has been visited before
+     (if (member node *visited-nodes*)
+	 ;; assign the nodes associated list to n
+	 (let ((n (assoc node *congestion-city-nodes*)))
+	   ;; If the node is the players current location
+	   (if (eql node *player-pos*)
+	       ;; then append an asterix to the node
+	       (append n '(*))
+	       ;; Otherwise return the associated list
+	       n))
+	 ;; Otherwise append a question mark to the node
+	 (list node '?)))
+   ;; mapcar 1 arguments
+   ;; Remove duplicate values
+   (remove-duplicates
+    ;; Combine visited nodes with 
+    (append *visited-nodes*
+	    ;; mapcan 1
+	    (mapcan (lambda (node)
+		      ;; mapcar 2
+		      (mapcar
+		       ;; mapcar 2 function
+		       #'car
+		       ;; mapcar arguments
+		       ;; A list of the edges adjacent to the current node
+		       (cdr (assoc node *congestion-city-edges*))))
+		    ;; mapcan 1 arguments
+		    *visited-nodes* )))))
+
+(defun known-city-edges ()
+  ;; mapcar 1
+  (mapcar
+   ;; mapcar 1 function
+   (lambda (node)
+     ;; combine node with
+     (cons node
+	   ;; mapcar 2
+	   (mapcar
+	    ;; mapcar 2 function
+	    (lambda (x)
+	      ;; If the node from the current edge is in visited nodes then return the edge
+	      (if (member (car x) *visited-nodes*)
+		  x
+		  ;; Otherwise return the node-edge list, stripping the cdr first
+		  (list (car x))))
+	    ;; mapcar 2 arguments
+	    ;; The edges adjacent to the current node
+	    (cdr (assoc node *congestion-city-edges*)))))
+   ;; mapcar 1 arguments
+   *visited-nodes*))
+
+
+(defun ingredients (order)
+  (mapcan (lambda (burger)
+	    (case burger
+	      (single '(patty))
+	      (double '(patty patty))
+	      (double-cheese '(patty patty cheese))))
+	  order))
+(ingredients '(single double-cheese double))
+
+
+(defun draw-known-city ()
+  (ugraph->png "known-city" (known-city-nodes) (known-city-edges)))
