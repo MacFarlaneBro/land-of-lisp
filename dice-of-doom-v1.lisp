8287be5d149856d8e28eaa0b4e276e1ece4b869f
Charlie MacFarlane Brodie <charliembrodie@gmail.com>
2019-01-29 21:03:40 +0000

    Added comments.

--- dice-of-doom-v1.lisp
+++ dice-of-doom-v1.lisp
@@ -42,7 +42,13 @@
 	     (loop repeat (- *board-size* y)
 		   ;; print a space
 		   do (princ "  "))
+	     ;; while x is below board size
 	     (loop for x below *board-size*
+		   ;; set hex equal to the value of the hex at teh x and y coordinates provided
 		   for hex = (aref board (+ x (* *board-size* y)))
+		   ;; print the letter of the player occupying the current hex
 		   do (format t "~a-~a " (player-letter (first hex))
+			      ;; print the number of dice on the hex
 			      (second hex))))))
+
+(draw-board (gen-board))
