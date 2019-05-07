a22bb020c5b467ed5ee691525680c5f0b3d0519d
Charlie MacFarlane Brodie <charliembrodie@gmail.com>
2019-05-07 22:06:52 +0100

    Started chapter 19.

---
+++ chapter19.lisp
@@ -0,0 +1,10 @@
+(load "dice-of-doom-v2.lisp")
+(load "web-server.lisp")
+(load "svg.lisp")
+
+(defparameter *board-width* 900)
+(defparameter *board-height* 500)
+(defparameter *board-scale* 64)
+(defparameter *top-offset* 3)
+(defparameter *dice-scale* 40)
+(defparameter *dot-size* 0.05)
